38829ee5b72775577fc2faae5d4854875cfe9979
fee-sah-yor <101174144+fee-sah-yor@users.noreply.github.com>
2022-08-26 11:27:41 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,29 @@
+var input = document.querySelector('input');
+var errorMsg = document.querySelector('h5');
+var errorImg = document.querySelector('.error');
+
+function btnClick() {
+    let accept = 0;
+    accept = input.value;
+
+    if (!accept.includes('@') || !accept.includes('.com')) {
+        input.setCustomValidity('invalid');
+        errorImg.classList.add('show');
+        errorMsg.classList.add('show');
+    } else if (accept.includes('@') || accept.includes('.com')) {
+        input.style.border = '1px solid hsl(0, 36%, 70%)'
+        input.value = " ";
+        errorImg.classList.remove('show');
+        errorMsg.classList.remove('show');
+    }
+}
+
+function addOnKeypress(event) {
+    if (event.keyCode === 13) {
+        btnClick();
+    }
+}
+
+document.querySelector('button').addEventListener('click', btnClick);
+
+document.querySelector('input').addEventListener('keypress', addOnKeypress);
